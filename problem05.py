0a0aa1eef29f3ab347db4fd5cd2db568a241a650
etort <etc.tortorici@gmail.com>
2015-04-09 08:58:33 -0600

    added gcd to problem 5

--- problem05.py
+++ problem05.py
@@ -1,5 +1,37 @@
 import numpy as np
+import copy
 
-def smallest_mult(L):
-## a function that finds the smallest multiple of all numbers in list L
+def LCM_simple(L):
+## a function that finds the smallest multiple of all numbers in list 'L'
+  try:
+    L.remove(1)
+  except:
+    pass
+  L0 = copy.copy(L)
+  L = np.array(L)
+  while sum(np.abs(np.abs(L[1:])-np.abs(L[:-1]))) != 0:
+    print L
+    n = np.argmin(L)
+    L[n] = L[n]+L0[n]
+  if L[0] == L[-1]:
+    return L[0]
+  else:
+    return 'I have failed you'
+
+def GCD(A,B):
+  if A > B:
+    R = A
+  else:
+    R = B
+  for r in range(2,R)[::-1]:
+    if A%r == 0 and B%r == 0:
+      gcd = r
+      break
+  return gcd
+
+#def LCM(L):
   
+
+if __name__ == '__main__':
+  L = range(1,21)
+  print LCM_simple(L)
